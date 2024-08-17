04a6cedd4e1f14ffc333c963491a8b9bb4c1b7c4
MJAI42 <marc.eljichi@gmail.com>
2024-08-17 14:25:03 +0400

    added new lines for the input

--- day_1/hello_world.py
+++ day_1/hello_world.py
@@ -1,6 +1,6 @@
 #Band name generator using input and storing variables
 print("Let's create a Band Name")
-city = input ("In which city did you grow up?")
-pet_name = input ("What is your pet's name?")
+city = input ("In which city did you grow up?\n")
+pet_name = input ("What is your pet's name?\n")
 print ("Here is your band name: " + city + " " + pet_name)
 
