03aa2023b3c9d12458b748a5764461fdff6ca4ab
MJAI42 <marc.eljichi@gmail.com>
2024-08-17 14:30:18 +0400

    final mini change

--- day_1/hello_world.py
+++ day_1/hello_world.py
@@ -3,4 +3,3 @@ print("Let's create a Band Name")
 city = input ("In which city did you grow up?\n")
 pet_name = input ("What is your pet's name?\n")
 print ("Here is your band name: " + city + " " + pet_name)
-
