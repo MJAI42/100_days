9a36dc0c99693e8ddeece5ba900f5bd22409a854
MJAI42 <marc.eljichi@gmail.com>
2024-09-01 21:49:27 +0400

    not yet done

--- day_8/ceaser_cipher.py
+++ day_8/ceaser_cipher.py
@@ -7,4 +7,21 @@ shift = int(input("Type the shift number:\n"))
 if direction == "encode":
     encoded_text = ""
     for char in text:
-
+        #check if it is a letter
+        count = 0
+        is_letter = False
+        while count < len(alphabet):
+            if char == alphabet[count]:
+                is_letter = True
+                count += 1
+            else:
+                count += 1
+        #if it is not a letter add it directly
+        if is_letter == False:
+            encoded_text += char
+        #if it is a letter do the shift
+        elif is_letter == True:
+                alpha_shift = (count + shift) % len(alphabet)
+                print(count)
+                encoded_text += alphabet[alpha_shift]
+print(f"Your encoded text is: {encoded_text}")
