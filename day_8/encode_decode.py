60d8810e9171706d1f446416220fe32f7dc1cba2
MJAI42 <marc.eljichi@gmail.com>
2024-09-02 16:55:38 +0400

    Ceaser Cypher work in progress

--- day_8/encode_decode.py
+++ day_8/encode_decode.py
@@ -1,16 +1,6 @@
-from encode import encode_text, decode_text 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
-if direction == "encode":
-     encoded_text = encode_text(text, shift)
-elif direction == "decode":
-     encoded_text = decode_text(text, shift)
-"""
+def encode_text (text, shift):   
     encoded_text = ""
     for char in text:
         #check if it is a letter
@@ -29,13 +19,13 @@ elif direction == "decode":
             encoded_text += char
         #if it is a letter do the shift
         elif is_letter == True:
-                alpha_shift = (position + shift - 1) % len(alphabet)
-                print(position)
-                encoded_text += alphabet[alpha_shift]
-"""
-"""
-if direction == "decode":
-    encoded_text = ""
+            alpha_shift = (position + shift - 1) % len(alphabet)
+            print(position)
+            encoded_text += alphabet[alpha_shift]
+    return (encoded_text)
+
+def decode_text (text, shift):   
+    decoded_text = ""
     for char in text:
         #check if it is a letter
         count = 0
@@ -50,12 +40,10 @@ if direction == "decode":
                 count += 1
         #if it is not a letter add it directly
         if is_letter == False:
-            encoded_text += char
+            decoded_text += char
         #if it is a letter do the shift
         elif is_letter == True:
-                alpha_shift = (position - shift - 1) % len(alphabet)
-                print(position)
-                encoded_text += alphabet[alpha_shift]
-""""
-
-print(f"Your encoded text is: {encoded_text}")
+            alpha_shift = (position - shift - 1) % len(alphabet)
+            print(position)
+            decoded_text += alphabet[alpha_shift]
+    return (decoded_text)
