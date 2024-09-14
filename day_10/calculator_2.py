afb7bfeac4cf232ca070c69fd28552e4fd8aa824
MJAI42 <marc.eljichi@gmail.com>
2024-09-14 19:28:22 +0400

    Calculator with a recursion

--- day_10/calculator_2.py
+++ day_10/calculator_2.py
@@ -1,3 +1,5 @@
+from art import logo
+
 def addition (a, b):
     return a + b 
 def substract (a, b):
@@ -12,26 +14,33 @@ operations = {
     "*": multiply,
     "/": divide,
 }
-switch = "y"
-first_num = int(input("Input the first number: "))
-while switch == "y":
-    for key in operations:
-        print(key)
-    operator = input("Pick an operation: ") 
-    second_num = int(input("Input the next number: "))
-    if operator == "+":
-        output = operations[operator](first_num, second_num)
-        print(f"{first_num} {operator} {second_num} = {output}")
-    elif operator == "-":
-        output = operations[operator](first_num, second_num)
-        print(f"{first_num} {operator} {second_num} = {output}")
-    elif operator == "*":
-        output = operations[operator](first_num, second_num)
-        print(f"{first_num} {operator} {second_num} = {output}")
-    elif operator == "/":
-        output = operations[operator](first_num, second_num)
-        print(f"{first_num} {operator} {second_num} = {output}")
-    first_num = output
-    switch = input(f"Type 'y' to continue calculating with {output}, or type 'n' to start a new calculation: ")
-    
-    
+def calc ():
+    switch = True
+    print(logo)
+    first_num = int(input("Input the first number: "))
+    while switch:
+        for key in operations:
+            print(key)
+        operator = input("Pick an operation: ") 
+        second_num = int(input("Input the next number: "))
+        if operator == "+":
+            output = operations[operator](first_num, second_num)
+            print(f"{first_num} {operator} {second_num} = {output}")
+        elif operator == "-":
+            output = operations[operator](first_num, second_num)
+            print(f"{first_num} {operator} {second_num} = {output}")
+        elif operator == "*":
+            output = operations[operator](first_num, second_num)
+            print(f"{first_num} {operator} {second_num} = {output}")
+        elif operator == "/":
+            output = operations[operator](first_num, second_num)
+            print(f"{first_num} {operator} {second_num} = {output}")
+        
+        choice = input(f"Type 'y' to continue calculating with {output}, or type 'n' to start a new calculation: ")
+        if choice == "y":
+            first_num = output
+        else:
+            switch = False
+            print("\n" * 100)
+            calc()
+calc()
