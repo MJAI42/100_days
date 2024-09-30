208689ae06c30b9eef83878b72abbcfc0a84c603
MJAI42 <marc.eljichi@gmail.com>
2024-09-30 21:39:43 +0400

    Higher Lower V2.0

--- day_14/higher_lower.py
+++ day_14/higher_lower.py
@@ -6,9 +6,9 @@ from game_data import data
 #chooses a candidate
 def shuffle(the_candidate):
     del the_candidate[:]
-    int_rand = random.randint(0, len(data))
-    for i in range (0, len(data[int_rand])):
-        the_candidate.append(data[int_rand][i])
+    int_rand = random.randint(0, len(data) - 1)
+    for key in data[int_rand]:
+        the_candidate.append(data[int_rand][key])
 
 #outputs users choice
 def a_or_b():
@@ -26,18 +26,44 @@ def a_or_b():
             print("I did not understand please try again.")
 
 #compares both candidates who has more follower count
-#def compare (the_candidates, ab):
+def compare (the_candidates, ab):
+    if the_candidates['a'][2] > the_candidates['b'][2]:
+        if ab == 'a':
+            return 1
+        else:
+            return 0
+    elif the_candidates['a'][2] < the_candidates['b'][2]:
+        if ab == 'b':
+            return 1
+        else:
+            return 0
      
 
 
 def the_game():
-    the_candidates = {'a':[], 'b':[]}
+    switch = True
+    score = 0
+    print("\n"*20)
     print(logo)
-    shuffle(the_candidates['a'])
-    print(f"Compare A: {the_candidates['a'][0]}, a {the_candidates['a'][2]}, from {the_candidates['a'][3]}")
-    print(vs)
-    shuffle(the_candidates['b'])
-    print(f"Compare B: {the_candidates['b'][0]}, a {the_candidates['b'][2]}, from {the_candidates['b'][3]}")
-    ab = a_or_b()
-
+    while switch:
+        the_candidates = {'a':[], 'b':[]}
+        shuffle(the_candidates['a'])
+        shuffle(the_candidates['b'])
+        print(f"Compare A: {the_candidates['a'][0]}, a {the_candidates['a'][2]}, from {the_candidates['a'][3]}")
+        print(vs)
+        print(f"Compare B: {the_candidates['b'][0]}, a {the_candidates['b'][2]}, from {the_candidates['b'][3]}")
+        ab = a_or_b()
+        winner = compare(the_candidates, ab)
+        print(winner)
+        if winner == 1:
+            score += 1
+            print("\n"*20)
+            print(logo)
+            print(f"You're right your current score is: {score}")
+        else:
+            score += 0 
+            print("\n"*20)
+            print(logo)
+            print(f"Sorry, that's wrong. Final score: {score}")
+            switch = False
 the_game()
