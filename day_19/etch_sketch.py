e4a1d21254d75f0ccf13845b5bc1f25c690c4704
MJAI42 <marc.eljichi@gmail.com>
2024-10-28 22:31:23 +0400

    Etch a Sketch

---
+++ day_19/etch_sketch.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+screen.listen()
+
+
+def move_fwd():
+    tim.forward(10)
+def move_bkw():
+    tim.backward(10)
+def move_left():
+    tim.left(10)
+def move_right():
+    tim.right(10)
+def clear_screen():
+    screen.clearscreen()
+screen.onkey(move_fwd, "w")
+screen.onkey(move_bkw, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.onkey(clear_screen, "c")
+screen.exitonclick()
